1fd92b41d51a9d9782c098264f3fd7e6fbbd2dbc
kshitij kumar <kshitijk83@gmail.com>
2019-06-16 01:34:38 +0530

    adding image support

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import _ from 'lodash';
+import './style.css';
+import Image from './bir.jpg';
 
 function component(){
     const element = document.createElement('div');
     element.innerHTML = _.join(['hello', 'webpack'], ' ');
+    element.classList.add('red');
+    var img= document.createElement('img');
+    img.src=Image;
+    element.appendChild(img);
     return  element;
 }
 
